155e43831d2ef8c49a9d7d2b96e01c26254078ab
Julio-SC <jhver2001@gmail.com>
2024-11-27 12:53:43 -0800

    Exercise Completed

--- README.md
+++ README.md
@@ -7,6 +7,7 @@
 
 Create an interactive accordion that expands and collapses content sections when the headers are clicked. Only one content section should be open. When a certain header is clicked, only its own content will slide down while the other content sections will collapse/slide up.
 
+
 ## Exercise 2 - AJAX
 
 Clicking on the **Load To-Do Items** button will pull the JSON data from `https://dummyjson.com/todos`. Use `$.ajax()` to fetch the data. After pulling the data and through the *success* method, output each todo as individual `<li>` items to the unordered list.
--- script.js
+++ script.js
@@ -1,3 +1,32 @@
 $(function() {
-  // your code here
+  $('.accordion-header').on('click', function() {
+    $(this).next('.accordion-content').slideToggle(
+      function () {
+        $(this).siblings('.accordion-content').hide('slow')
+      }
+    )
+  })
+
+
+  $('.todos').find('button').on('click', function() {
+   $.ajax({
+    url: 'https://dummyjson.com/todos',
+    type: 'GET',
+    success: function(response) {
+      const data = response.todos
+      data.forEach(element => {
+        const todo =element.todo
+        $('ul').append(`<li>${todo} </li>`)
+
+        console.log(todo)
+      });
+
+    },
+    error: function(error) {
+
+    }
+
+   })
+ })
 })
+
